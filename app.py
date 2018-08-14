748ab2a21bd707f2dc32f70a58d08c482cb125ff
iobrien <iobrien@pdx.edu>
2018-08-13 18:18:43 -0700

    sqllite 3 does not work on cloud, reverting to original idea of doing an emotional detection of a mindfullness app using framework for homework #5

--- app.py
+++ app.py
@@ -1,40 +1,26 @@
 """
 flask app for routing request to landing page and movie review page 
 """
-from flask import Flask, render_template, redirect, url_for, request, jsonify
-from Model import Model
-from model_sqlite3 import model
+from flask import Flask, render_template, jsonify
+from my_model import model
 
 app = Flask(__name__)       # our Flask app
 model = model()             #instantiate dictionary
 """
-Function decorator === app.route('/',index())
+Fnction decorator === app.route('/',index())
 """
-
-#landing page for website
 @app.route('/')
 def index():
-  return render_template('home.html') 
+    #"""landing page"""
+    return "A PLACE FOR MOVIE REWIVEWS...an experiment in web development" 
  
-#this route will allow users to read all current reviews in the "database"
-#had issues formating so used jsonify method
-@app.route('/review', methods = ['GET'])
+@app.route('/review')
 def review():
-  return jsonify(model.select())
+   #publish reviews to webpage
+   #this function will allow the reivews to be printed/published on the web app
+    return render_template('layout.html', model = model) 
 
-#this route will allow users to add reviews to the database via a form
-#redirects to reviews page when completed 
-@app.route('/post_review', methods = ['GET'])
-def post_review():
-    return render_template('index.html')
 
-#this route handles the adding of the reviews and then redirects the user to all 
-#of the reviews once completed
-@app.route('/add_review', methods = ['POST'])
-def add_review():
-  model.insert(request.form['title'],request.form['year_released'],request.form['genre'],request.form['rating'],request.form['reviewer'],request.form['comments'])
-  return redirect(url_for('review'))
-    
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = 4996, debug = True)
+    app.run(host = '0.0.0.0', port = 4996) 
