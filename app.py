346fa7323eb0d7800923e083cf3e6243a1b7924b
iobrien <iobrien@pdx.edu>
2018-08-13 19:18:38 -0700

    basic bones of app working time to bring in ML_GCP_translate_API to finish up the job

--- app.py
+++ app.py
@@ -1,26 +1,27 @@
 """
-flask app for routing request to landing page and movie review page 
+flask app english to french translator using GCP_ML_API 
 """
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, redirect, request, url_for, jsonify
 from my_model import model
 
-app = Flask(__name__)       # our Flask app
+app = Flask(__name__)       #our Flask app
 model = model()             #instantiate dictionary
-"""
-Fnction decorator === app.route('/',index())
-"""
+
+#home page for website 
 @app.route('/')
 def index():
-    #"""landing page"""
-    return "A PLACE FOR MOVIE REWIVEWS...an experiment in web development" 
- 
-@app.route('/review')
-def review():
-   #publish reviews to webpage
-   #this function will allow the reivews to be printed/published on the web app
-    return render_template('layout.html', model = model) 
+    return render_template('home.html')
 
+#this page will prompt user to type their sentence in english that they wish to
+#translate to french, it will be sent to translator GCP_ML_API and spit out
+#the french translation
+@app.route('/form', methods = ['GET'])
+def form():
+    return render_template('form.html')
 
+@app.route('/ML_transform')
+def ML_transform():
+    return "ok"
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = 4996) 
+    app.run(host = '0.0.0.0', port = 4996, debug = True)
